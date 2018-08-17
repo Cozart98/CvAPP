0f1393de98bd811d5a1d09232fc95685ac82b906
Cozart98 <chihaoui1098@gmail.com>
2018-08-17 16:00:16 +0200

    adding phone, mail, adress, linkedin on MeActivity

--- app/src/main/java/com/example/cozart/cvapp/MainActivity.java
+++ app/src/main/java/com/example/cozart/cvapp/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.cozart.cvapp;
 
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -39,14 +37,7 @@ public class MainActivity extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
     }
 
